4907782f0afbb3c81cc84e8df621210f67adedbc
Willingham <tanjinxiao25@gmail.com>
2021-01-29 21:41:01 +0800

    注释

--- cartographer/common/port.h
+++ cartographer/common/port.h
@@ -1,19 +1,10 @@
-/*
- * Copyright 2016 The Cartographer Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+/*
+    common/port.h 主要功能
+    1. 使用std::Lround对浮点数进行四舍五入取整运算
+    2. 使用gzip对字符串压缩与解压缩
+
+*/
 #ifndef CARTOGRAPHER_COMMON_PORT_H_
 #define CARTOGRAPHER_COMMON_PORT_H_
 
